72b0d3c64fc820649d6a886bd94c40e1904256a7
Eddy Retana <eddyretanav14@gmail.com>
2026-06-01 11:57:16 -0600

    modifying firewall.tf ukraine geo control

--- modules/appsec/firewall.tf
+++ modules/appsec/firewall.tf
@@ -3,5 +3,5 @@ resource "akamai_appsec_ip_geo" "prod" {
   config_id                  = akamai_appsec_configuration.security_configuration.config_id
   security_policy_id         = akamai_appsec_security_policy.security-policy.security_policy_id
   mode                       = "block"
-  ukraine_geo_control_action = "Deny"
+  ukraine_geo_control_action = "deny"
 }
