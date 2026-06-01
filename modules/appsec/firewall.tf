1df470ef5b2ede4873ce13d662cf5936f86f1d40
Eddy Retana <eddyretanav14@gmail.com>
2026-06-01 11:33:10 -0600

    adding firewall.tf to appsec module

---
+++ modules/appsec/firewall.tf
@@ -0,0 +1,7 @@
+// IP/GEO/ASN Firewall
+resource "akamai_appsec_ip_geo" "prod" {
+  config_id                  = akamai_appsec_configuration.security_configuration.config_id
+  security_policy_id         = akamai_appsec_security_policy.prod.security_policy_id
+  mode                       = "block"
+  ukraine_geo_control_action = "none"
+}
